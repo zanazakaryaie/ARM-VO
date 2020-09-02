5d59319cc09fd459731a6b62e6c184c4e037091c
zanazakaryaie <zana.zakaryaie@gmail.com>
2020-09-02 23:21:53 +0100

    cleaner

--- include/pose.hpp
+++ include/pose.hpp
@@ -1,26 +1,13 @@
-#ifndef POSE_HPP_INCLUDED
-#define POSE_HPP_INCLUDED
-
+#pragma once
 
 #include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
 
+void decomposeEssentialMat( const cv::Mat& E, cv::Mat &R1, cv::Mat &R2, cv::Mat &t );
 
-void decomposeEssentialMat( cv::Mat E, cv::Mat &R1, cv::Mat &R2, cv::Mat &t );
+int cheiralCheck(const cv::Mat &P1, const cv::Mat &points1, const cv::Mat &points2,
+                 float dist, cv::Mat &mask, cv::Mat &X);
 
+void recoverPose( cv::Mat F, const cv::InputArray _points1, const cv::InputArray _points2,
+                 const cv::Mat &cameraMatrix, cv::OutputArray _R, cv::OutputArray _t, cv::Mat &mask, cv::Mat &X);
 
-void cvTriangulate_Points(CvMat* projMatr1, CvMat* projMatr2, CvMat* projPoints1, CvMat* projPoints2, CvMat* points4D);
-
-
-void triangulate_Points( cv::Mat &projMatr1, cv::Mat &projMatr2,
-                         cv::InputArray _projPoints1, cv::InputArray _projPoints2,
-                         cv::OutputArray _points4D );
-
-
-int cheiralCheck(cv::Mat &P1, cv::Mat &points1, cv::Mat &points2, float dist, cv::Mat &mask, cv::Mat &X);
-
-
-void recoverPose( cv::Mat E, cv::InputArray _points1, cv::InputArray _points2, cv::Mat &cameraMatrix,
-                  cv::OutputArray _R, cv::OutputArray _t, cv::Mat &mask, cv::Mat &X);
-
-
-#endif // POSE_HPP_INCLUDED
