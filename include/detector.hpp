891e2db96e5c5e9e21f520a739f55bc69afb4523
zanazakaryaie <zana.zakaryaie@gmail.com>
2020-09-02 23:21:12 +0100

    cleaner. used cerr

--- include/detector.hpp
+++ include/detector.hpp
@@ -1,8 +1,25 @@
-#ifndef DETECTOR_HPP_INCLUDED
-#define DETECTOR_HPP_INCLUDED
+#pragma once
 
 #include <vector>
+#include <iostream>
 #include <opencv2/features2d.hpp>
+
+
+class gridFASTdetector
+{
+public:
+    void setMaxFeatures(const int n);
+    void setThreshold(const int thresh);
+    void setGridSize(const cv::Size& gridSize);
+    bool detect( const cv::Mat& image, std::vector<cv::Point2f>& points);
+
+private:
+    int nFeatures;
+    int threshold;
+    cv::Size grid;
+};
+
+
 
 struct ResponseComparator
 {
@@ -12,20 +29,21 @@ struct ResponseComparator
     }
 };
 
-// Selects the N strongest keypoints detected based on their response
-void keepStrongest( int N, std::vector<cv::KeyPoint>& keypoints );
+void keepStrongest( int N, std::vector<cv::KeyPoint>& keypoints ); // Selects the N strongest keypoints detected based on their response
 
-class gridFASTdetector
+
+
+class GridAdaptedFeatureDetectorInvoker : public cv::ParallelLoopBody
 {
 public:
-    gridFASTdetector(int n, int thresh, int grid_x, int grid_y);
-    void detect( const cv::Mat& image, std::vector<cv::Point2f>& points);
+    GridAdaptedFeatureDetectorInvoker(const cv::Mat& image, std::vector<cv::KeyPoint>& keypoints, const int maxPerCell, const int gridRows, const int gridCols, const int thresh, cv::Mutex* kptLock);
+    void operator() (const cv::Range& range) const;
 
 private:
-    int nFeatures;
-    int threshold;
-    int nRows;
-    int nCols;
+    int gridRows_, gridCols_;
+    int maxPerCell_;
+    int threshold_;
+    std::vector<cv::KeyPoint>& keypoints_;
+    const cv::Mat& image_;
+    cv::Mutex* kptLock_;
 };
-
-#endif // DETECTOR_HPP_INCLUDED
